4ff5f64a92fdfc303c67e703af4fc7a67d7406f2
John Wells <molekewl2017@gmail.com>
2017-08-30 03:37:28 -0700

    Changed text app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AudioEngine from './components/AudioContext';
+import AudioEngine from './components/AudioEngine';
 
 class App extends Component {
   render() {
@@ -12,7 +12,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Make sure to turn DOWN <code>Your Speakers!!</code> before playing.
           <AudioEngine />
         </p>
       </div>
